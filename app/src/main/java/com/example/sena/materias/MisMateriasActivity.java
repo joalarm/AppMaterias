10ab34caf17ab5bac07f11fddd63b05281150991
Jose Alejandro Arias <jaariasme@gmail.com>
2015-12-11 14:57:58 -0500

    Cambio estetico al listview

--- app/src/main/java/com/example/sena/materias/MisMateriasActivity.java
+++ app/src/main/java/com/example/sena/materias/MisMateriasActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
 import android.widget.SimpleCursorAdapter;
 
 public class MisMateriasActivity extends AppCompatActivity implements EncabezadoFragment.OnFragmentInteractionListener {
@@ -20,6 +21,7 @@ public class MisMateriasActivity extends AppCompatActivity implements Encabezado
     private ListView lstViewMaterias;
     private Cursor cursorLstMaterias;
     private SimpleCursorAdapter simpleAdapter;
+    private RelativeLayout layout;
     SharedPreferences spref;
 
     @Override
@@ -31,6 +33,7 @@ public class MisMateriasActivity extends AppCompatActivity implements Encabezado
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         dbManager = new DBManager(this);
+        layout = (RelativeLayout)findViewById(R.id.lytSinMaterias);
         spref = getSharedPreferences("usuario", Context.MODE_PRIVATE);
         if(spref.getInt("id",-1)==-1)
         {
@@ -65,19 +68,27 @@ public class MisMateriasActivity extends AppCompatActivity implements Encabezado
 
     public void cargar_materias_profesor(){
         cursorLstMaterias = dbManager.consultaAsignaturasProfesor(spref.getInt("id",0));
-        String [] from = new String[] {"nombre", "_id"};
-        int [] to = new int[] {R.id.txt1, R.id.txt2};
-        simpleAdapter = new SimpleCursorAdapter(this, R.layout.listamaterias, cursorLstMaterias,from, to, 0);
-        lstViewMaterias = (ListView) findViewById(R.id.lstMaterias);
-        lstViewMaterias.setAdapter(simpleAdapter);
+        if(cursorLstMaterias.getCount() > 0) {
+            String[] from = new String[]{"nombre", "_id"};
+            int[] to = new int[]{R.id.txt1, R.id.txt2};
+            simpleAdapter = new SimpleCursorAdapter(this, R.layout.listamaterias, cursorLstMaterias, from, to, 0);
+            lstViewMaterias = (ListView) findViewById(R.id.lstMaterias);
+            lstViewMaterias.setAdapter(simpleAdapter);
+            layout.setVisibility(View.GONE);
+            lstViewMaterias.setVisibility(View.VISIBLE);
+        }
     }
 
     public void cargar_materias_alumno(){
         cursorLstMaterias = dbManager.consultaInscripciones(spref.getInt("id",0));
-        String [] from = new String[] {"nombre_asignatura", "nombre_profesor"};
-        int [] to = new int[] {R.id.txt1, R.id.txt2};
-        simpleAdapter = new SimpleCursorAdapter(this, R.layout.listamaterias, cursorLstMaterias,from, to, 0);
-        lstViewMaterias = (ListView) findViewById(R.id.lstMaterias);
-        lstViewMaterias.setAdapter(simpleAdapter);
+        if(cursorLstMaterias.getCount() > 0) {
+            String[] from = new String[]{"nombre_asignatura", "nombre_profesor"};
+            int[] to = new int[]{R.id.txt1, R.id.txt2};
+            simpleAdapter = new SimpleCursorAdapter(this, R.layout.listamaterias, cursorLstMaterias, from, to, 0);
+            lstViewMaterias = (ListView) findViewById(R.id.lstMaterias);
+            lstViewMaterias.setAdapter(simpleAdapter);
+            layout.setVisibility(View.GONE);
+            lstViewMaterias.setVisibility(View.VISIBLE);
+        }
     }
 }
